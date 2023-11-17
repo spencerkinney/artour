52a920f7f5c28d0fecef6074a83423b60f23dca8
spencerkinney <spencer_kinney@yahoo.com>
2023-11-16 21:09:37 -0600

    refactor index.js file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { MarkerProvider } from "./context";
 import App from "./App";
 
-createRoot(document.getElementById("root")).render(<App/>);
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
+  <MarkerProvider>
+    <App />
+  </MarkerProvider>
+);
