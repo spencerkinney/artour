2dace947b67e75953c6926ea2d0828f1c4dd9c9d
spencerkinney <spencer_kinney@yahoo.com>
2023-11-18 10:15:13 -0600

    complete

--- src/Home.js
+++ src/Home.js
@@ -3,45 +3,26 @@ import { ARCanvas, ARMarker } from "@artcom/react-three-arjs";
 import { useTexture, Plane, Box } from "@react-three/drei";
 import { useNavigate } from "react-router-dom";
 
-const markerPatterns = [
-  "/data/pattern-tuan.patt",
-  "/data/pattern-1.patt",
-  "/data/pattern-2.patt",
-  "/data/pattern-3.patt",
-  "/data/pattern-4.patt",
-  "/data/pattern-5.patt",
-];
+const markerPatterns = ["/data/pattern-tuan.patt"];
 
-function ArtworkBox({ inputTexture, isVisible }) {
-  const artworkTexture = useTexture(inputTexture); // Load the artwork image
-  const artworkWidth = 1.5; // Example width
-  const artworkHeight = 2; // Example height
-  const frameDepth = 0.05; // The depth of the frame
-  const frameWidth = 0.1; // The width of the frame's border
-  const groupRotation = [(Math.PI / 2) * -1, 0, 0];
+function ArtworkBox({ inputTexture }) {
+  const artworkTexture = useTexture(inputTexture);
+  // Adjust these dimensions as needed
+  const artworkWidth = 1.5; 
+  const artworkHeight = 2; 
+
+  // This ensures the artwork is centered and aligned with the marker
+  const groupRotation = [(Math.PI / 2) * -1, 0, 0]; 
 
   return (
-    <mesh isVisible={isVisible} position={[0, 0, 0]}>
-      <group rotation={groupRotation}>
-        <Plane
-          args={[artworkWidth, artworkHeight]}
-          position={[0, 0, frameDepth]}
-        >
-          <meshStandardMaterial attach="material" map={artworkTexture} />
-        </Plane>
-        <Box
-          args={[
-            artworkWidth + frameWidth,
-            artworkHeight + frameWidth,
-            frameDepth,
-          ]}
-        >
-          <meshStandardMaterial attach="material" color={"brown"} />
-        </Box>
-      </group>
-    </mesh>
+    <group rotation={groupRotation}>
+      <Plane args={[artworkWidth, artworkHeight]} position={[0, 0, 0]}>
+        <meshStandardMaterial attach="material" map={artworkTexture} />
+      </Plane>
+    </group>
   );
 }
+
 
 function Home() {
   const artworkRefs = useRef(); // Ref for ArtworkBox
@@ -67,17 +48,14 @@ function Home() {
     >
       <ambientLight />
       <pointLight position={[10, 10, 0]} intensity={10.0} />
-
-      {markerPatterns.map((patternUrl, index) => (
-        <ARMarker
-          key={index}
-          debug={false}
-          //params={{ smooth: true }}
-          type={"pattern"}
-          patternUrl={patternUrl}
-          onMarkerFound={() => handleMarkerFound(index)}
-        />
-      ))}
+      <ARMarker
+        debug={false}
+        params={{ smooth: true }}
+        type={"pattern"}
+        patternUrl={"/data/pattern-2.patt"}
+      >
+        <ArtworkBox inputTexture={"/data/exhibit2.jpg"}/>
+      </ARMarker>
     </ARCanvas>
   );
 }
